f7123fdb0969ca5e545d2a636aa8e88b6d2b81a7
권진오 <kjo1333@gmail.com>
2023-06-16 15:22:45 +0900

    env test

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
-  },
   images: {
     domains: ["images.unsplash.com"],
   },
--- pages/login.tsx
+++ pages/login.tsx
@@ -7,10 +7,6 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 
-(function () {
-  console.log(process.env);
-})();
-
 export default function Home() {
   const router = useRouter();
 
