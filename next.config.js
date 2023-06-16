488382b830732914f6eedbb56419440dd56342c5
권진오 <kjo1333@gmail.com>
2023-06-16 11:25:17 +0900

    task 동적으로 수정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
